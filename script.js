8bac3ccf0574eeb542fa5baf3b5369e9cad8e87a
Josh Hsieh <jtr860830@gmail.com>
2023-06-07 02:19:05 +0800

    fix: typo

--- script.js
+++ script.js
@@ -37,100 +37,10 @@ function compute() {
       matrix[i][j] = Number(document.getElementById(`cell-${i}-${j}`).value);
     }
   }
-  const result = hungarian(matrix);
+  console.log(matrix);
+  const result = computeMunkres(matrix);
+  console.log(result);
   const resultContainer = document.getElementById("result");
-  resultContainer.innerHTML = "<h5>Minimum Cost Assignment:</h5>";
-  let outputHTML =
-    '<table class="striped"><thead><tr><th>Worker</th><th>Job</th></tr></thead><tbody>';
-  for (let i = 0; i < result.length; i++) {
-    outputHTML += `<tr><td>${result[i][0] + 1}</td><td>${
-      result[i][1] + 1
-    }</td></tr>`;
-  }
-  outputHTML += "</tbody></table>";
-  resultContainer.innerHTML += outputHTML;
-}
-
-function hungarian(costs) {
-  // subtract min of rows
-  for (let i = 0; i < costs.length; i++) {
-    const min = Math.min(...costs[i]);
-    for (let j = 0; j < costs[i].length; j++) {
-      costs[i][j] -= min;
-    }
-  }
-
-  // subtract min of columns
-  for (let j = 0; j < costs[0].length; j++) {
-    let min = Infinity;
-    for (let i = 0; i < costs.length; i++) {
-      min = Math.min(min, costs[i][j]);
-    }
-    for (let i = 0; i < costs.length; i++) {
-      costs[i][j] -= min;
-    }
-  }
-
-  // creating the mask
-  let mask = costs.map((row) => row.map((cell) => (cell === 0 ? 1 : 0)));
-
-  let coveredColumns = [];
-  let coveredRows = [];
-
-  //Cover all zero in a row or column that doesn't have any other covered zero
-  for (let i = 0; i < mask.length; i++) {
-    for (let j = 0; j < mask[i].length; j++) {
-      if (
-        mask[i][j] === 1 &&
-        !coveredRows.includes(i) &&
-        !coveredColumns.includes(j)
-      ) {
-        coveredRows.push(i);
-        coveredColumns.push(j);
-        break;
-      }
-    }
-  }
-
-  while (coveredColumns.length < costs[0].length) {
-    let min = Infinity;
-    for (let i = 0; i < costs.length; i++) {
-      for (let j = 0; j < costs[i].length; j++) {
-        if (!coveredRows.includes(i) && !coveredColumns.includes(j)) {
-          min = Math.min(min, costs[i][j]);
-        }
-      }
-    }
-
-    for (let i = 0; i < costs.length; i++) {
-      for (let j = 0; j < costs[i].length; j++) {
-        if (!coveredRows.includes(i) && !coveredColumns.includes(j)) {
-          costs[i][j] -= min;
-        } else if (coveredRows.includes(i) && coveredColumns.includes(j)) {
-          costs[i][j] += min;
-        }
-      }
-    }
-
-    mask = costs.map((row) => row.map((cell) => (cell === 0 ? 1 : 0)));
-    coveredColumns = [];
-    coveredRows = [];
-
-    for (let i = 0; i < mask.length; i++) {
-      for (let j = 0; j < mask[i].length; j++) {
-        if (
-          mask[i][j] === 1 &&
-          !coveredRows.includes(i) &&
-          !coveredColumns.includes(j)
-        ) {
-          coveredRows.push(i);
-          coveredColumns.push(j);
-          break;
-        }
-      }
-    }
-  }
-
-  // return the minimum cost assignment
-  return coveredRows.map((row, i) => [row, coveredColumns[i]]);
+  const cost = result.reduce((acc, cur) => acc + matrix[cur[0]][cur[1]], 0);
+  resultContainer.innerHTML = `<h5>Minimum Cost: ${cost}</h5>`;
 }
